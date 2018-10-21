71a10e5b43424db8ffcda3a1f8f8a4859c318ac0
rizkiazis <rizkiajismaulana@gmail.com>
2018-10-21 10:00:26 +0700

    mengubah folder

---
+++ latihan1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    int p, l, luas;
+cout <<"masukan panjang: ";
+cin >> p;
+cout << "masukan lebar: ";
+    cin >> l;
+    luas = p * l;
+
+cout << "luas persegi panjang adalah: " << luas << endl;
+return 0;
+}
